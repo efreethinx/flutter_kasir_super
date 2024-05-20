70993eaf0f1683418655d6fbfa054f559be76d33
efreethinx <efreethinx@gmail.com>
2024-05-20 15:29:33 +0700

    P2 1

--- lib/features/settings/pages/splash/page.dart
+++ lib/features/settings/pages/splash/page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kasir_super/core/core.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3)).then(
+      (value) => Navigator.pushNamedAndRemoveUntil(
+        context,
+        '/home',
+        (route) => false,
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
